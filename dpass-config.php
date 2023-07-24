86d7d3b170011287007bfb0a5149ad33a87c81d3
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2023-07-24 09:20:07 -0500

    Create dpass-config.php

---
+++ dpass-config.php
@@ -0,0 +1,12 @@
+<?php      
+$password="password";
+//password to login
+$csvdb="MyVault.csv";
+//csv database filename(must exist)
+$configured=true;
+//set this to true
+$showdonate=true;
+//set it to false to hide it
+$sha1prefix="mypw";
+//added in front of the password string when hashing the password
+?>
